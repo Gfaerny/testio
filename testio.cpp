0e629d8e386101052abc2ce31885ab35d0ea830a
gfaerny <120312426+Gfaerny@users.noreply.github.com>
2024-07-09 20:34:48 +0330

    Update testio.cpp

--- testio.cpp
+++ testio.cpp
@@ -2,8 +2,14 @@
 int main(int argc, char* argv[])
 {
 std::string in , out;
-std
+ if ( argc == 4)
+{
+std::cout << "Enter input :"<<std::endl;
+std::cin>>in;
+std::cout << "Now enter the output :"<<std::endl;
+std::cin>>out;
 
+}
 
 
 
