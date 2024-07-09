295a3aadebd84ad6e439e0e2955dae267f6f72ca
gfaerny <120312426+Gfaerny@users.noreply.github.com>
2024-07-09 12:49:47 +0330

    Update bs.sh

--- bs.sh
+++ bs.sh
@@ -1,7 +1,31 @@
-# this script is for install testio for linux and macos guy
+# this script is for build testio for linux and macos guy
 #u # ##########################################
 #s # You can use it like command and get it arg
 #a # example :
 #g # testio sciptname.cpp 
 #e # input 
 #### output
+
+os=$(lsb_release -si)
+base=$(lsb_release -si | tr '[:upper:]' '[:lower:]')
+
+case $base in
+    arch)
+        
+        sudo pacman -S --noconfirm gcc
+        ;;
+    debian)
+        
+        sudo apt-get update
+        sudo apt-get install -y g++
+        ;;
+    fedora)
+        
+        sudo dnf install -y gcc-c++
+        ;;
+    *)
+        echo "Unsupported distribution: $os"
+        exit 1
+        ;;
+esac
+g++ -o testio testio.cpp
