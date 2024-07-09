977897b462c413f414b70546651da6ed062ffbd2
gfaerny <120312426+Gfaerny@users.noreply.github.com>
2024-07-09 02:05:00 -0700

    Update bs.sh

--- bs.sh
+++ bs.sh
@@ -1,1 +1,7 @@
-#this script used for linux guy 
+# this script is for install testio for linux and macos guy
+#u # ##########################################
+#s # You can use it like command and get it arg
+#a # example :
+#g # testio sciptname.cpp 
+#e # input 
+#### output
