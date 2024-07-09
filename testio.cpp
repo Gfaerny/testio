ab5bab2e17e6f91dde10f24405bf46626565a8d5
gfaerny <120312426+Gfaerny@users.noreply.github.com>
2024-07-09 21:10:40 +0330

    Update testio.cpp

--- testio.cpp
+++ testio.cpp
@@ -1,18 +1,66 @@
 #include <iostream>
+
+
+std::string runCommand(const std::string& command) 
+{
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(command.c_str(), "r");
+    if (!pipe) 
+    {
+        std::cerr << "Error: Unable to execute command" << std::endl;
+        return "";
+    }
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL) 
+   {
+        result += buffer;
+    }
+    pclose(pipe);
+    return result;
+}
+
+
 int main(int argc, char* argv[])
+
 {
 std::string in , out;
- if ( argc == 4)
+std::string script = argv[1];
+ if ( argc == 2)
 {
 std::cout << "Enter input :"<<std::endl;
 std::cin>>in;
 std::cout << "Now enter the output :"<<std::endl;
 std::cin>>out;
+    std::string command = script + " < " + input;
+    std::string appOutput = runCommand(command);
+
+    // Read the expected output from the file
+    std::ifstream file(output);
+    std::string expectedOutput((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+    // Compare the output with the expected output
+    if (appOutput == expectedOutput) 
+    {
+        std::cout << "Output matches the expected output." << std::endl;
+    } else 
+    {
+        std::cout << "Output does not match the expected output." << std::endl;
+    }
+
+
+
+
+
 
 }
 
 
+else 
+{
+std::cerr<<"You have to enter command with argument( your script that want to test);
 
+return 1;
+}
 
 
 }
